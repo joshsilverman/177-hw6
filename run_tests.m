c9a540f68a782dab1c064aaa95c20d85e8174142
Josh Silverman <joshs.silverman@gmail.com>
2011-04-29 19:25:22 -0400

    diary and testing file

---
+++ run_tests.m
@@ -0,0 +1,15 @@
+% call hw6 tests
+
+addpath "tests"
+
+test_powerN()
+test_array_front9()
+test_bunnyEars()
+test_count7()
+test_countClumps()
+test_factorial()
+test_fibonacci()
+test_groupSum()
+test_has23()
+test_maxSpan()
+test_stringSplosion()
